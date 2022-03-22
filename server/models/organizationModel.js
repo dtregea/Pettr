bf939c17693e58bccd9e3c4558eda7b0ad4b4909
dtregea <daniel.tregea@gmail.com>
2022-03-21 20:29:03 -0400

    Pet and Organization models created

--- server/models/organizationModel.js
+++ server/models/organizationModel.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
+
+const organizationSchema = mongoose.Schema({
+  apiId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: String,
+  email: String,
+  phone: String,
+  address1: String,
+  address2: String,
+  city: String,
+  state: String,
+  zip: Number,
+  country: String,
+  url: String,
+  website: String,
+  adoptionPolicy: String,
+  adoptionUrl: String,
+  photos: Array,
+});
+
 module.exports = mongoose.model("Organization", organizationSchema);
--- server/models/petModel.js
+++ server/models/petModel.js
@@ -1,1 +1,42 @@
+const mongoose = require("mongoose");
+
+const petSchema = mongoose.Schema({
+  apiId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  org: {
+    ref: "organization",
+  },
+  apiOrgId: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  species: String,
+  breed: String,
+  size: String,
+  gender: String,
+  age: Number,
+  color: String,
+  coat: String,
+  status: String,
+  contactEmail: String,
+  contactPhone: String,
+  address1: String,
+  city: String,
+  state: String,
+  zip: Number,
+  country: String,
+  attributes: Array,
+  environment: Array,
+  photos: Array,
+  description: String,
+  videos: Array,
+  tags: Array,
+});
+
 module.exports = mongoose.model("Pet", petSchema);
