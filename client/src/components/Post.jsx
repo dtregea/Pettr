9b690a53383626fa984d99751dff9145ff272975
dtregea <daniel.tregea@gmail.com>
2022-03-27 13:57:44 -0400

    Post toggles visibility of CommentBox

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -7,6 +7,7 @@ import RepeatIcon from "@mui/icons-material/Repeat";
 //import { HeartSwitch } from "@anatoliygatt/heart-switch";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import CommentBox from "./CommentBox";
 
 function Post({
   text,
@@ -24,30 +25,36 @@ function Post({
   repostedBy,
 }) {
   const [likes, setLikes] = useState(likeCount);
-  const [liked, setLiked] = useState(isLiked);
+  const [likedByUser, setLikedByUser] = useState(isLiked);
   const [reposts, setReposts] = useState(repostCount);
-  const [reposted, setReposted] = useState(isReposted);
+  const [repostedByUser, setRepostedByUser] = useState(isReposted);
+  const [comments, setComments] = useState(commentCount);
+  const [showCommentBox, setShowCommentBox] = useState(false);
+
+  useEffect(() => {
+    setShowCommentBox(showCommentBox);
+  }, [showCommentBox]);
 
   useEffect(() => {
     setLikes(likeCount);
-  }, []);
+  }, [likeCount]);
 
   useEffect(() => {
-    setLiked(isLiked);
-  }, []);
+    setLikedByUser(isLiked);
+  }, [isLiked]);
 
   useEffect(() => {
     setReposts(repostCount);
-  }, []);
+  }, [repostCount]);
 
   useEffect(() => {
-    setReposted(isReposted);
-  }, []);
+    setRepostedByUser(isReposted);
+  }, [isReposted]);
 
   const [waiting, setWaiting] = useState(false);
 
   async function toggleLike() {
-    let route = liked ? "unlike" : "like";
+    let route = likedByUser ? "unlike" : "like";
     if (!waiting) {
       setWaiting(true);
       const response = await fetch(
@@ -64,10 +71,10 @@ function Post({
       if (fetchedData) {
         if (fetchedData.status === "success") {
           setLikes(fetchedData.data.likeCount);
-          setLiked(fetchedData.data.isLiked);
+          setLikedByUser(fetchedData.data.isLiked);
         } else if (fetchedData.status === "fail") {
-          setLikes(liked ? likes - 1 : likes + 1);
-          setLiked(fetchedData.data.isLiked);
+          setLikes(likedByUser ? likes - 1 : likes + 1);
+          setLikedByUser(fetchedData.data.isLiked);
         }
         setWaiting(false);
       }
@@ -75,7 +82,7 @@ function Post({
   }
 
   async function toggleRepost() {
-    let route = reposted ? "unrepost" : "repost";
+    let route = repostedByUser ? "unrepost" : "repost";
     if (!waiting) {
       setWaiting(true);
       const response = await fetch(
@@ -93,60 +100,74 @@ function Post({
         console.log(fetchedData);
         if (fetchedData.status === "success") {
           setReposts(fetchedData.data.repostCount);
-          setReposted(fetchedData.data.isReposted);
+          setRepostedByUser(fetchedData.data.isReposted);
         } else if (fetchedData.status === "fail") {
-          setReposts(reposted ? reposts - 1 : reposts + 1);
-          setReposted(fetchedData.data.isReposted);
+          setReposts(repostedByUser ? reposts - 1 : reposts + 1);
+          setRepostedByUser(fetchedData.data.isReposted);
         }
         setWaiting(false);
       }
     }
   }
-  return (
-    <div className={`post ${trendingView && "trending"}`}>
-      <div className="post-avatar">
-        <Avatar src={user.avatar} />
-      </div>
-      <div className="post-body">
-        <div className="post-header">
-          <div className="post-headerText">
-            <span className="post-headerSpecial">
-              {repostedBy != null && repostedBy + " reposted"}
-            </span>
 
-            <h3>
-              {user.displayname}{" "}
-              <span className="post-headerSpecial">
-                {verified && <VerifiedIcon className="post-badge" />} @
-                {user.username}
-              </span>
-            </h3>
-          </div>
-          <div className="post-headerDescription">
-            <p>{text}</p>
-          </div>
+  function toggleCommentBox() {
+    setShowCommentBox(!showCommentBox);
+  }
+
+  function updateCommentCount(commentCount) {
+    console.log("updating");
+    setComments(commentCount);
+  }
+  return (
+    <div className="post-container">
+      <div className={`post ${trendingView && "trending"}`}>
+        <div className="post-avatar">
+          <Avatar src={user.avatar} />
         </div>
-        <img src={image} alt="" />
-        <div className="post-footer">
-          <div>
-            <ChatBubbleOutlineIcon fontSize="small" /> {commentCount}
+        <div className="post-body">
+          <div className="post-header">
+            <div className="post-headerText">
+              <span className="post-headerSpecial">
+                {repostedBy != null && repostedBy + " reposted"}
+              </span>
+
+              <h3>
+                {user.displayname}{" "}
+                <span className="post-headerSpecial">
+                  {verified && <VerifiedIcon className="post-badge" />} @
+                  {user.username}
+                </span>
+              </h3>
+            </div>
+            <div className="post-headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div onClick={toggleRepost}>
-            {!reposted && <RepeatIcon fontSize="small" />}
-            {reposted && (
-              <RepeatIcon fontSize="small" style={{ color: "green" }} />
-            )}{" "}
-            {reposts}
-          </div>
-          <div onClick={toggleLike}>
-            {!liked && <FavoriteBorderIcon fontSize="small" />}
-            {liked && (
-              <FavoriteIcon fontSize="small" style={{ color: "red" }} />
-            )}{" "}
-            {likes}
+          <img src={image} alt="" />
+          <div className="post-footer">
+            <div onClick={toggleCommentBox}>
+              <ChatBubbleOutlineIcon fontSize="small" /> {comments}
+            </div>
+            <div onClick={toggleRepost}>
+              {!repostedByUser && <RepeatIcon fontSize="small" />}
+              {repostedByUser && (
+                <RepeatIcon fontSize="small" style={{ color: "green" }} />
+              )}{" "}
+              {reposts}
+            </div>
+            <div onClick={toggleLike}>
+              {!likedByUser && <FavoriteBorderIcon fontSize="small" />}
+              {likedByUser && (
+                <FavoriteIcon fontSize="small" style={{ color: "red" }} />
+              )}{" "}
+              {likes}
+            </div>
           </div>
         </div>
       </div>
+      {showCommentBox && (
+        <CommentBox updateCommentCount={updateCommentCount} postId={id} />
+      )}
     </div>
   );
 }
