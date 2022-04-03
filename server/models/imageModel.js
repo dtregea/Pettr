baabf0df6bd4f91a71fc4831bf71b5ded9dbfcc8
dtregea <daniel.tregea@gmail.com>
2022-04-02 22:53:01 -0400

    Timestamps added to images

--- server/models/imageModel.js
+++ server/models/imageModel.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const imageSchema = mongoose.Schema({
-  name: String,
-  desc: String,
-  img: {
-    data: Buffer,
-    contentType: String,
+const imageSchema = mongoose.Schema(
+  {
+    img: {
+      data: Buffer,
+      contentType: String,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Image", imageSchema);
