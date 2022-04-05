549e5834ad3fcfa9b2310859d7f26cea0999215c
dtregea <daniel.tregea@gmail.com>
2022-04-04 21:39:59 -0400

    Pets and Profile tab created

---
+++ client/src/components/Pets.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import "../styles/Pets.css";
+function Pets(props) {
+  return <div className="pets">pets</div>;
+}
+
+export default Pets;
---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import "../styles/Profile.css";
+function Profile(props) {
+  return <div className="profile">profile</div>;
+}
+
+export default Profile;
