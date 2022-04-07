a1991b68c952ad38c2849385657ad00d08d89369
dtregea <daniel.tregea@gmail.com>
2022-04-06 20:56:14 -0400

    Profile component fetches and displays user information

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,24 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Profile.css";
 import Feed from "./Feed";
+import { Avatar } from "@mui/material";
 function Profile(props) {
   const [posts, setPosts] = useState([]);
-  const [profileInfo, SetProfileInfo] = useState({});
+  const [userInfo, setUserInfo] = useState({});
   useEffect(() => {
-    let route = `/api/users/${localStorage.getItem("id")}/posts`;
+    async function fetchUserInfo() {
+      const response = await fetch(
+        `http://localhost:5000/api/users/${localStorage.getItem("id")}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+      const fetchedData = await response.json();
+      if (fetchedData) {
+        console.log(fetchedData.data.user);
+        setUserInfo(fetchedData.data.user);
+      }
+    }
+    fetchUserInfo();
+  }, []);
+
+  useEffect(() => {
     async function fetchPosts() {
-      const response = await fetch(`http://localhost:5000${route}`, {
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      });
+      const response = await fetch(
+        `http://localhost:5000/api/users/${localStorage.getItem("id")}/posts`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
       const fetchedData = await response.json();
       if (fetchedData) {
         setPosts(fetchedData.data.posts);
       }
     }
     fetchPosts();
-  }, [props.isPostModal]);
+  }, []);
 
   return (
     <div className="profile">
@@ -28,7 +50,37 @@ function Profile(props) {
         <h2>Profile</h2>
       </div>
 
-      <div className="profile-box"></div>
+      <div className="profile-box">
+        {userInfo.avatar && (
+          <div className="profile-avatar">
+            <Avatar
+              sx={{ height: "70px", width: "70px" }}
+              src={userInfo.avatar}
+            />
+          </div>
+        )}
+
+        <div className="profile-info">
+          <div className="profile-names">
+            <div className="profile-name">{userInfo.displayname}</div>
+            <span>@{userInfo.username}</span>
+          </div>
+          <ul className="profile-numbers">
+            <li className="profile-details">
+              <span className="profile-label">Tweets</span>
+              <span className="profile-number">0</span>
+            </li>
+            <li className="profile-details">
+              <span className="profile-label">Following</span>
+              <span className="profile-number">0</span>
+            </li>
+            <li className="profile-details">
+              <span className="profile-label">Followers</span>
+              <span className="profile-number">0</span>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       {/* Profile posts */}
       <Feed posts={posts} showModal={props.showModal} />
