344afaef2690ed9fa04f930ac99a5014d1fea3d0
dtregea <daniel.tregea@gmail.com>
2022-04-14 17:55:22 -0400

    Styling added to login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "../api/axios";
 import useAuth from "../hooks/useAuth";
+import "../styles/Login.css";
 const Login = () => {
   // const [username, setUsername] = useState("");
   const [username, setUsername] = useState("");
@@ -37,25 +38,39 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={loginUser}>
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          placeholder="username"
-        />
-        <br />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+    <div class="login">
+      <div class="login-container">
+        <div class="login-form-container">
+          <form class="login-form" onSubmit={loginUser}>
+            <span class="login-form-title">Log In</span>
+            <div class="login-form-field">
+              <input
+                value={username}
+                class="login-form-input"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
+            </div>
+            <div class="login-form-field">
+              <input
+                class="login-form-input"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="Password"
+              />
+            </div>
+            <input className="login-form-submit" type="submit"></input>
+
+            <div class="register-now-container">
+              <span class="no-account-text">Don’t have an account?</span>
+              <a href="/register" class="register-now-text">
+                Sign up now
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/Login.css";
 function Register() {
+  const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+  const REGISTER_URL = "/register";
   const [displayname, setDisplayName] = useState("");
   const [username, setUsername] = useState("");
-  //const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   async function registerUser(event) {
     event.preventDefault();
-    console.log("register fired");
     const response = await fetch("http://localhost:5000/api/users", {
       method: "POST",
       headers: {
@@ -33,43 +35,52 @@ function Register() {
   }
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={registerUser}>
-        <input
-          value={displayname}
-          onChange={(e) => setDisplayName(e.target.value)}
-          type="text"
-          placeholder="Name"
-          name="name"
-        />
-        <br />
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          placeholder="Username"
-          name="username"
-        />
-        {/* <br />
-         <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Email"
-          name="email"
-        /> */}
-        <br />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-          name="password"
-        />
-        <br />
-        <input type="submit" value="Register" />
-      </form>
+    <div class="login">
+      <div class="login-container">
+        <div class="login-form-container">
+          <form class="login-form" onSubmit={registerUser}>
+            <span class="login-form-title">Create An Account</span>
+            <div class="login-form-field">
+              <input
+                class="login-form-input"
+                value={displayname}
+                onChange={(e) => setDisplayName(e.target.value)}
+                type="text"
+                placeholder="Name"
+                name="name"
+              />
+            </div>
+            <div class="login-form-field">
+              <input
+                class="login-form-input"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </div>
+            <div class="login-form-field">
+              <input
+                class="login-form-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </div>
+            <input className="login-form-submit" type="submit"></input>
+
+            <div class="register-now-container">
+              <span class="no-account-text">Already have an account?</span>
+              <a href="/login" class="register-now-text">
+                Log in now
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
